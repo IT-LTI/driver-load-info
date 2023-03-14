6d8805fcc766c094d768eed47809f8e9f2a98485
IT-LTI <126505085+IT-LTI@users.noreply.github.com>
2023-03-14 14:11:46 -0400

    Update application.py

--- application.py
+++ application.py
@@ -20,8 +20,8 @@ def index(order_id, movement_id, stop_id, tractor_id):
             
             insert_into_bol_table(stop_id, bol_numbers[idx], weights[idx], pieces[idx], idx+1, unique_id)
 
-        # return render_template('index.html', stop_id = stop_id, order_id = order_id, movement_id = movement_id, tractor_id = tractor_id)
-        return render_template('success.html', stop_id = stop_id, order_id = order_id, movement_id = movement_id, tractor_id = tractor_id)
+        return render_template('index.html', stop_id = stop_id, order_id = order_id, movement_id = movement_id, tractor_id = tractor_id)
+        #return render_template('success.html', stop_id = stop_id, order_id = order_id, movement_id = movement_id, tractor_id = tractor_id)
     
     elif request.method == 'GET':
         return render_template('index.html', stop_id = stop_id, order_id = order_id, movement_id = movement_id, tractor_id = tractor_id)
