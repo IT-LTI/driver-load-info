b15eb10299a69be32763e071535010afe0e64117
IT-LTI <126505085+IT-LTI@users.noreply.github.com>
2023-03-14 14:08:09 -0400

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,5 +2,5 @@ import os
 
 DB_HOST = 'driver-load-info-db.clxkywsjsxsy.us-east-2.rds.amazonaws.com'
 DB_USER = 'admin'
-DB_PASSWORD = '5$kaPF6m28!&'
+DB_PASSWORD = os.environ.get("DB_SECRET_CODE")
 DB_NAME = 'dli'
